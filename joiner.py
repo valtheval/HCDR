2c0f10aa277ad81f245c29b0ff545289a22b4928
vmasdeu <valentin.masdeu@gmail.com>
2018-08-04 22:33:57 +0200

    ADD, joiner file

---
+++ joiner.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import pandas as pd
+
+class Joiner():
+
+    def __init__(self):
+        pass
+
+
+    def join(self, right, left, on, how='inner'):
+        # TODO
+        pass
+
+    def map(self, df):
+        #TODO
+        # Fonction qui met à plat df en 1 l par ce qu'on veut (ici application). Peut être qu'il faudra 1 fonction par dataframe
+        # map_bureau, map_previous etc...
+        pass
